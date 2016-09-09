fcff81f6c49f8adf1049caa6e9dc8bda687b7e14
elsen1905 <elshan.z@code.edu.az>
2016-09-09 16:06:49 +0400

    elsen

--- product.php
+++ product.php
@@ -24,40 +24,60 @@
 	<br>
 
 	<div class="row ">
-	<div class="col-md-3 " >
+	<div class="col-md-3  " >
 		<div style="background-color:white" class="elsen text-center">
 			<img  src="assets/images/2.jpg" alt="">
 			<h4 >Date Palm</h4>
 			<p class=" p">$407</p>
 			<b  style="font-size:11pt; color:#787878">Return : 20%-30% per year</b>
-			<span></span>
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Date Palm</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	<div class="col-md-3">
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img src="assets/images/15.jpg" alt="">
 			<h4 >Avocado</h4>
 			<p class=" p">$203</p>
 			<b  style="font-size:11pt; color:#787878">Return : 15% per year</b>
-
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Avocado</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	<div class="col-md-3">
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img src="assets/images/14.jpg" alt="">
 			<h4 >Fragrant Roots</h4>
 			<p class=" p">$814</p>
 			<b  style="font-size:11pt; color:#787878">Return : 15%</b>
-
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Fragrant Roots</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 		<div class="col-md-3">
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img src="assets/images/13.jpg" alt="">
 			<h4 ">Deli Water Apple</h4>
 			<p class=" p">$122</p>
 			<b  style="font-size:11pt; color:#787878">Return : 7%-20% per year</b>
-
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Deli Water Apple</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	</div>
@@ -66,40 +86,60 @@
 	<br>
 	<div class="row ">
 	<div class="col-md-3" >
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img  src="assets/images/2.jpg" alt="">
 			<h4 >Date Palm</h4>
 			<p class=" p">$407</p>
 			<b  style="font-size:11pt; color:#787878">Return : 20%-30% per year</b>
-			<span></span>
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Date Palm</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	<div class="col-md-3">
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img src="assets/images/2.jpg" alt="">
 			<h4 >Avocado</h4>
 			<p class=" p">$203</p>
 			<b  style="font-size:11pt; color:#787878">Return : 15% per year</b>
-
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Avocado</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	<div class="col-md-3">
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img src="assets/images/2.jpg" alt="">
 			<h4 >Fragrant Roots</h4>
 			<p class=" p">$814</p>
 			<b  style="font-size:11pt; color:#787878">Return : 15%</b>
-
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Fragrant Roots</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 		<div class="col-md-3">
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img src="assets/images/12.jpg" alt="">
 			<img class="sold-out" src="assets/images/sold-out.png">
 			<h4 ">Banana</h4>
 			<p class=" p">$1.017</p>
 			<b  style="font-size:11pt; color:#787878">Return : 15%-20% per year</b>
-
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Banana</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	</div>
@@ -108,41 +148,61 @@
 	<br>
 	<div class="row ">
 	<div class="col-md-3" >
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img  src="assets/images/11.jpg" alt="">
 			<img class="sold-out" src="assets/images/sold-out.png">
 			<h4 >Peanut</h4>
 			<p class=" p">$1.213</p>
 			<b  style="font-size:11pt; color:#787878">Return : 9%-13% per 6 months</b>
-			<span></span>
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Peanut</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	<div class="col-md-3">
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img src="assets/images/9.jpg" alt="">
 			<img class="sold-out" src="assets/images/sold-out.png">
 			<h4 >Longan</h4>
 			<p class=" p">$122</p>
 			<b  style="font-size:11pt; color:#787878">Return : 12%-76$ per year</b>
-
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Longan</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	<div class="col-md-3">
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img src="assets/images/8.jpg" alt="">
 			<h4 >Durian</h4>
 			<p class=" p">$407</p>
 			<b  style="font-size:11pt; color:#787878">Return : 19%-47%</b>
-
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Durian</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 		<div class="col-md-3">
-		<div style="background-color:white" class="text-center">
+		<div style="background-color:white" class=" elsen text-center">
 			<img src="assets/images/1.jpg" alt="">
 			<h4 ">Olive</h4>
 			<p class=" p">$285</p>
 			<b  style="font-size:11pt; color:#787878">Return : 12%-20% per year</b>
-
+			<div class="hoverr">
+				<h2 style="margin-top:80px;">Olive</h2>
+				<form action="">
+					<button class="btnn">INVEST NOW</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	</div>
