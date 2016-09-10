a13ac3ef98a14a1f982271ad9b3819bd9df01407
Fidan Abbasova <fidan.a@code.edu.az>
2016-09-10 18:41:38 +0400

    sign an about

---
+++ signin.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html class="no-js" lang=""> 
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>TRAMASS | Sign in </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css"> <!--Diger yerde istifade olunsa yukleyerik -->
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body> 
+        <section id="signin">      
+        <div  class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <h2>Sign in</h2>
+                    <p>Please enter your username and password</p>
+                    <form>
+                        <div class="form-group">
+                            <label for="inputEmail1">Username or Email</label>
+                            <input type="email" class="form-control" id="inputEmail1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputPassword1">Password</label>
+                            <input type="password" class="form-control" id="inputPassword1" placeholder="">
+                        </div>
+                        <a href="forgot_password.php">Forgot Password?</a><br>
+                        <input type="submit" value="SIGN IN" class="btn btn-success btn-lg text-center col-md-12 btn-2">
+                        <p class="text-center">OR</p>
+                        <button type="submit" class="btn btn-primary btn-lg text-center col-md-12 btn-2"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> LOGIN WITH FACEBOOK</a></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </section> 
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="js/vendor/jquery-1.11.2.min.js"></script>
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html class="no-js" lang=""> 
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>TRAMASS | Sign up</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css"> <!--Diger yerde istifade olunsa yukleyerik -->
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        
+        <section id="signup">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4">
+                        <h2>Sign up</h2>
+                        <p>Please enter your username and password</p>
+                        <form>
+                            <div class="form-group">
+                                <label for="inputName">Your Name</label>
+                                <input type="text" class="form-control" id="inputName" placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail1">Email</label>
+                                <input type="email" class="form-control" id="inputEmail1" placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPassword1">Password</label>
+                                <input type="password" class="form-control" id="inputPassword1" placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPassword2">Confirm Password</label>
+                                <input type="password" class="form-control" id="inputPassword2" placeholder="">
+                            </div>
+                            <button type="submit" class="btn btn-success btn-lg text-center col-md-12 btn-2">SIGN IN</button>
+                            <p class="text-center">OR</p>
+                            <button type="submit" class="btn btn-primary btn-lg text-center col-md-12 btn-2"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> LOGIN WITH FACEBOOK</a></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="js/vendor/jquery-1.11.2.min.js"></script>
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
