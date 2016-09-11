b83940312de876d1fffa8af88e735916a868f526
FARIDKH <farid.q@code.edu.az>
2016-09-11 15:59:52 +0400

    index.php Bitdi

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js" lang=""> 
+<html> 
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -8,16 +8,362 @@
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+           <style>
+            html,body{
+                font-family: "Montserrat", sans-serif;
+            }
+        </style>
     </head>
     <body>
         
+        <section id="scroll_navbar">
+          <div class="container-fluid">
+             <div class="container">
+                  <div class="row">
+                    <nav class="navbar">
+                          <div class="container-fluid">
+                                <div class="navbar-header col-md-3 col-sm-12 col-xs-12 ">
+                                    <a class="navbar-brand" href="#"><span>TRA</span>MASS</a>
+                                </div>
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li class="active"><a href="#">HOME</a></li>
+                                    <li><a href="#">ABOUT US</a></li>
+                                    <li><a href="#">LOG IN</a></li> 
+                                    <li><a href="#">SIGN UP</a></li>
+                                </ul>
+                          </div>
+                    </nav>
+                </div>
+              </div>
+          </div>
+           
+        </section>
+        
+        <!-- header start -->
+        
+        <section id="header">
+                  <div class="col-md-12">
+                      <div class="row">
+                        <nav class="navbar">
+                              <div class="container-fluid">
+                                    <div class="navbar-header">
+
+                                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                            <a class="navbar-brand" href="#"><span>T</span></a>
+                                        </button>
+
+                                        <a class="hidden-xs  navbar-brand" href="#"><span>TRA</span>MASS</a>
+                                    
+                                    </div>
+
+                                    <div class="collapse navbar-collapse" id="myNavbar">
+
+                                        <ul class="nav navbar-nav navbar-right">
+                                            <li class="active"><a href="#">HOME</a></li>
+                                            <li><a href="#">ABOUT US</a></li>
+                                            <li><a href="#">LOG IN</a></li> 
+                                            <li><a href="#">SIGN UP</a></li>
+                                        </ul>
+
+                                    </div>
+                              </div>
+                        </nav>
+                       </div>
+                           <div class="row header_heading">
+                               <h1>IMPROVE <span>YO</span>UR CROP!</h1>
+                               <div class="hidden-sm hidden-xs divider"></div>
+                           </div>
+                           <div class="row">
+                               <h3>IMPLEMENTING NEW TECHNOLOGIES FOR AGRICULTURE GROWTH</h3>
+                           </div>
+                           <div class="row button_part_header">
+                               <button class="btn defaultButton">READ MORE
+                                   <div></div>
+                               </button>
+                               <button class="btn successButton">JOIN US
+                               </button>
+                          </div>
+                      <div class="row">
+                          <div id="header_carousel" class="slide_carousel carousel slide" data-ride="carousel">    
+
+                                  <div class="carousel-inner" role="listbox">
+                                    <div class="item active">
+                                      <img src="img/header/page-1_slide01.jpg" alt="">
+                                    </div>
+
+                                    <div class="item">
+                                      <img src="img/header/page-1_slide02.jpg" alt="">
+                                    </div>
+
+                                    <div class="item">
+                                      <img src="img/header/page-1_slide03.jpg" alt="">
+                                    </div>
+                                </div>
+                          </div>
+                      </div>
+                  </div>
+        </section>
+        
+        <!-- header ending -->
+        
+        
+        <!-- services start-->    
+       
+        <section id="services">
+            <div class="container">
+                <div class="col-md-8 col-sm-12 col-xs-12  header_service">
+                    <div class="row">
+                        <h1>Recommendations to minimize the effects of the practices on agricultural crop <br> production.</h1>
+                        <div class="divider"></div>
+                    </div>
+                </div>
+                <div class="service_part">
+                    <div class="col-md-6 col-sm-12 col-xs-12 left_service">
+                        <div class="row">
+                            <h1>SERVICES</h1>
+                        </div>
+                        <div class="row">
+                            <p>We provide inspection, testing, and risk management services across the whole agricultural supply chain. From seeds to crops, our expert team performs independent assessments of quality of your agricultural products and processes. We offer the best agronomic advice possible based on our knowledge.</p>
+                        </div>
+                        <div class="row">
+                            <button class="btn">
+                              <div></div>
+                               READ MORE
+                                
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-xs-12 right_service">
+                        <ul>
+                            <li>Agricultural Cargo Inspection</li>
+                            <li>Agricultural Services A-Z</li>
+                            <li>Food Aid Program Inspection</li>
+                            <li>GMO Seed Testing Services</li>
+                            <li>Grain Inspection and Testing</li>
+                            <li>Oilseeds Testing</li>
+                            <li>Precision Farming</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- services ending-->
+        
+        <!-- sep1 start-->
+        <section id="sep1">
+           <div class="row">
+                <div class="col-md-3 col-sm-6  sep_fruit_1">
+                   <div class="fruit_slide_effect_01"></div>
+                   <div class="content_sep_fruit_01">
+                        <h1>CORN</h1>
+                        <div class="divider"></div>
+                        <span>READ MORE</span>
+                   </div>
+                </div>
+                <div class="col-md-3 col-sm-6 sep_fruit_2">
+                  <div class="fruit_slide_effect_02"></div>
+                   <div class="content_sep_fruit_02">
+                        <h1>CUCUMBERS</h1>
+                        <div class="divider"></div>
+                        <span>READ MORE</span>
+                   </div>
+                   
+                </div>
+                <div class="col-md-3 col-sm-6 sep_fruit_3">
+                    <div class="fruit_slide_effect_03"></div>
+                    <div class="content_sep_fruit_03">
+                        <h1>TOMATOES</h1>
+                        <div class="divider"></div>
+                        <span>READ MORE</span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 sep_fruit_4">
+                    <div class="fruit_slide_effect_04"></div>
+                    <div class="content_sep_fruit_04">
+                        <h1>PEAS</h1>
+                        <div class="divider"></div>
+                        <span>READ MORE</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- sep1 ending-->
+        
+        <section id="coreValues">
+            <div class="container">
+                <div class="col-md-8 col-sm-12 col-xs-12  header_core">
+                    <div class="row">
+                        <h1>Our mission is profitable growth through superior customer service, innovation, quality and commitment.</h1>
+                        <div class="divider"></div>
+                    </div>
+                </div>
+                <div class="content_core">
+                    <div class="col-md-6 col-sm-12 col-xs-12 left_core">
+                        <div class="row">
+                            <h1>CORE VALUES</h1>
+                        </div>
+                        <div class="row">
+                            <p>Our core values guide us in how we conduct ourselves—personally and professionally. In this ever-changing world of agriculture, our core values are constant. Our values underlie our work, how we interact with each other and which strategies we employ to fulfill our mission.</p>
+                        </div>
+                        <div class="row">
+                            <button class="btn">
+                                <div></div>
+                            READ MORE</button>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-xs-12 right_core">
+                        <img src="img/services/page-1_img08.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+       
+        
+        <section id="gallery_header">
+            
+            <h1>GALLERY</h1>
+            <div class="divider"></div>
+        </section>
     
+        <section id="gallery">
+        
+            <div class="grid">
+              <div class="grid-item">
+                  <img src="img/gallery/page-1_img09_original.jpg" alt="">
+                  <i class="fa fa-plus fa-3x"></i>
+              </div>
+              <div class="grid-item grid-item--width2">
+                  <img src="img/gallery/page-1_img10_original.jpg" alt="">
+                  <i class="fa fa-plus fa-3x"></i>
+              </div>
+              <div class="grid-item">
+                  <img src="img/gallery/page-1_img11_original.jpg" alt="">
+                  <i class="fa fa-plus fa-3x"></i>
+              </div>
+              <div class="grid-item grid-item--width2">
+                  <img src="img/gallery/page-1_img12_original.jpg" alt="">
+                  <i class="fa fa-plus fa-3x"></i>
+              </div>
+              <div class="grid-item">
+                  <img src="img/gallery/page-1_img14_original.jpg" alt="">
+                  <i class="fa fa-plus fa-3x"></i>
+              </div>
+              <div class="grid-item">
+                  <img src="img/gallery/page-1_img16_original.jpg" alt="">
+                  <i class="fa fa-plus fa-3x"></i>
+              </div>
+              <div class="grid-item grid-item--width2">
+                  <img src="img/gallery/page-1_img13_original.jpg" alt="">
+                  <i class="fa fa-plus fa-3x"></i>
+              </div>
+              <div class="grid-item grid-item--width2">
+                  <img src="img/gallery/page-1_img15_original.jpg" alt="">
+                  <i class="fa fa-plus fa-3x"></i>
+              </div>
+              
+              
+            </div>
+            
+        </section>       
+       
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="js/vendor/jquery-1.11.2.min.js"></script>
         <script src="js/vendor/bootstrap.min.js"></script>
+        
+        <script src="https://unpkg.com/masonry-layout@4.1/dist/masonry.pkgd.min.js"></script>
         <script src="js/main.js"></script>
+               <script>
+        $('.grid').masonry({
+        // options
+        itemSelector: '.grid-item',
+//        fitWidth: true
+        });
+        </script>
+        <script> 
+        
+               
+        
+        
+         
+            
+//        var windowWidth = $(window).width();
+//        function masonryLayout() {
+//            var blocks = document.querySelector("#gallery .row div")
+//            var cols = 4, newleft, newtop;
+//            for(var i = 1; i < blocks.length; i++){
+//                if (i % cols == 0) {
+//                    newtop = (blocks[i-cols].offsetTop + blocks[i-cols].offsetHeight);
+//                    blocks[i].style.top = newtop+"px";
+//                } else {
+//                    if(blocks[i-cols]){
+//                        newleft = (blocks[i-cols].offsetTop + blocks[i-cols].offsetHeight);
+//                        blocks[i].style.top = newleft+"px";
+//                    }
+//                    newleft = (blocks[i-1].offsetLeft + blocks[i-1].offsetWidth);
+//                    blocks[i].style.left = newleft+"px";	
+//                }
+//            }
+//        }
+//        window.addEventListener("load", masonryLayout, false);
+//        window.addEventListener("resize", masonryLayout, false);
+//            
+            
+        
+
+            
+       function fruitSlide(hoverContent,slideeffect){
+           $(hoverContent).mouseenter(function(){
+               $(slideeffect).slideDown('300', function() {
+                   $(this).fadeIn();
+               });
+           })
+           
+           $(hoverContent).mouseleave(function(){
+               $(slideeffect).slideUp('300', function() {
+                   $(this).fadeOut();
+               });
+           })
+       } 
+       
+       fruitSlide('.sep_fruit_1','.fruit_slide_effect_01')
+       fruitSlide('.sep_fruit_2','.fruit_slide_effect_02')
+       fruitSlide('.sep_fruit_3','.fruit_slide_effect_03')
+       fruitSlide('.sep_fruit_4','.fruit_slide_effect_04')
+        console.log('.....no...error..:)')
+        </script>
     </body>
 </html>
+<script>
+
+
+$(document).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+    if(wScroll > $('.header_heading').offset().top){
+        
+        $( "#scroll_navbar" ).fadeIn( "fast", function() {
+            // Animation complete.
+        });
+        
+	};
+    if(wScroll < $('.header_heading').offset().top){
+        
+        $( "#scroll_navbar" ).fadeOut( "fast", function() {
+            // Animation complete.
+        });
+        
+	};
+    
+})
+
+
+</script>
