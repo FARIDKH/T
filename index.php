d519621c188cd0f9a58addc10f42ee8dfc4f77b8
senem qenberova <senem qenberova>
2016-09-14 21:45:27 +0400

    90 faiz bitdi

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html> 
+<html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -23,7 +23,7 @@
         </style>
     </head>
     <body>
-        
+
         <section id="scroll_navbar">
           <div class="container-fluid">
              <div class="container">
@@ -34,89 +34,123 @@
                                     <a class="navbar-brand" href="#"><span>TRA</span>MASS</a>
                                 </div>
                                 <ul class="nav navbar-nav navbar-right">
-                                    <li class="active"><a href="#">HOME</a></li>
-                                    <li><a href="#">ABOUT US</a></li>
-                                    <li><a href="#">LOG IN</a></li> 
-                                    <li><a href="#">SIGN UP</a></li>
+                                    <li class="active"><a href="index.php">HOME</a></li>
+                                    <li><a href="about.php">ABOUT US</a></li>
+                                    <li><a href="signin.php">LOG IN</a></li>
+                                    <li><a href="signup.php">SIGN UP</a></li>
                                 </ul>
                           </div>
                     </nav>
                 </div>
               </div>
           </div>
-           
+
         </section>
-        
+
         <!-- header start -->
-        
+
         <section id="header">
-                  <div class="col-md-12">
-                      <div class="row">
-                        <nav class="navbar">
-                              <div class="container-fluid">
-                                    <div class="navbar-header">
+              <div class="col-md-12">
+                  <div class="row">
+                    <nav class="navbar">
+                          <div class="container-fluid">
+                                <div class="navbar-header">
 
-                                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                                            <a class="navbar-brand" href="#"><span>T</span></a>
-                                        </button>
+                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                        <a class="navbar-brand" href="#"><span>T</span></a>
+                                    </button>
 
-                                        <a class="hidden-xs  navbar-brand" href="#"><span>TRA</span>MASS</a>
-                                    
-                                    </div>
+                                    <a class="hidden-xs  navbar-brand" href="#"><span>TRA</span>MASS</a>
 
-                                    <div class="collapse navbar-collapse" id="myNavbar">
+                                </div>
 
-                                        <ul class="nav navbar-nav navbar-right">
-                                            <li class="active"><a href="#">HOME</a></li>
-                                            <li><a href="#">ABOUT US</a></li>
-                                            <li><a href="#">LOG IN</a></li> 
-                                            <li><a href="#">SIGN UP</a></li>
-                                        </ul>
+                                <div class="collapse navbar-collapse" id="myNavbar">
 
-                                    </div>
-                              </div>
-                        </nav>
-                       </div>
-                           <div class="row header_heading">
-                               <h1>IMPROVE <span>YO</span>UR CROP!</h1>
-                               <div class="hidden-sm hidden-xs divider"></div>
-                           </div>
-                           <div class="row">
-                               <h3>IMPLEMENTING NEW TECHNOLOGIES FOR AGRICULTURE GROWTH</h3>
-                           </div>
-                           <div class="row button_part_header">
-                               <button class="btn defaultButton">READ MORE
-                                   <div></div>
-                               </button>
-                               <button class="btn successButton">JOIN US
-                               </button>
-                          </div>
-                      <div class="row">
-                          <div id="header_carousel" class="slide_carousel carousel slide" data-ride="carousel">    
+                                  <ul class="nav navbar-nav navbar-right">
+                                      <li class="active"><a href="index.php">HOME</a></li>
+                                      <li><a href="about.php">ABOUT US</a></li>
+                                      <li><a href="signin.php">LOG IN</a></li>
+                                      <li><a href="signup.php">SIGN UP</a></li>
+                                  </ul>
 
-                                  <div class="carousel-inner" role="listbox">
-                                    <div class="item active">
-                                      <img src="img/header/page-1_slide01.jpg" alt="">
-                                    </div>
-
-                                    <div class="item">
-                                      <img src="img/header/page-1_slide02.jpg" alt="">
-                                    </div>
-
-                                    <div class="item">
-                                      <img src="img/header/page-1_slide03.jpg" alt="">
-                                    </div>
                                 </div>
                           </div>
+                    </nav>
+                   </div>
+                       <div class="row header_heading">
+                           <h1>IMPROVE <span>YO</span>UR CROP!</h1>
+                           <div class="hidden-sm hidden-xs divider"></div>
+                       </div>
+                       <div class="row">
+                           <h3>IMPLEMENTING NEW TECHNOLOGIES FOR AGRICULTURE GROWTH</h3>
+                       </div>
+                       <div class="row button_part_header">
+                           <button class="btn defaultButton">READ MORE
+                               <div></div>
+                           </button>
+                           <button class="btn successButton">JOIN US
+                           </button>
+                      </div>
+                  <div class="row">
+                      <div id="header_carousel" class="slide_carousel carousel slide" data-ride="carousel">
+
+                              <div class="carousel-inner" role="listbox">
+                                <div class="item active">
+                                  <img src="img/header/page-1_slide01.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                  <img src="img/header/page-1_slide02.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                  <img src="img/header/page-1_slide03.jpg" alt="">
+                                </div>
+                            </div>
                       </div>
                   </div>
+              </div>
         </section>
-        
+
         <!-- header ending -->
-        
-        
-        <!-- services start-->    
-       
+
+        <!-- welcome start -->
+
+        <section id="welcome">
+            <div class="container first_container">
+              <div class="row col-lg-12  col-md-12  col-sm-12">
+                <p class="starting_prgph"> We continue to develop our capacities. Even more fruits and vegetables will be grown and gathered in the nearest future. The total number of our livestock is also increasing.</p>
+                <div class="line"> </div>
+              </div>
+                   <h1 class="col-md-4 col-sm-6 col-xs-6"> WELCOME</h1>
+            </div>
+               <div class="row">
+                 <div class="col-lg-6 col-md-12 col-xs-12 first_img">
+                   <img src="img/page-1_img01.jpg" alt="" />
+                   <div class=" col-lg-12 col-md-12 col-sm-12  first_prgph">
+                     <p>
+                       We are a world-leading provider of beef and agricultural products. We own and operate a strategic balance of properties, feedlots and farms comprising around 10 million hectares of land. This equates to roughly 1% of country’s land mass. Having been established in 1826, we are the oldest continuously operating company in the country.
+                     </p>
+                   </div>
+                 </div>
+                 <div class="col-lg-6 col-md-12 col-md-12 second_img">
+                   <img src="img/page-1_img02.jpg" alt="" />
+                   <div class=" col-lg-12 col-md-12  col-sm-12 second_prgph">
+                     <p>
+                       We are a world-leading provider of beef and agricultural products. We own and operate a strategic balance of properties, feedlots and farms comprising around 10 million hectares of land. This equates to roughly 1% of country’s land mass. Having been established in 1826, we are the oldest continuously operating company in the country.
+                     </p>
+                   </div>
+                 </div>
+                 <div class="row">
+                   <button>READ </button>
+                 </div>
+                </div>
+
+        </section>
+        <!-- welcome ending -->
+
+        <!-- services start-->
+
         <section id="services">
             <div class="container">
                 <div class="col-md-8 col-sm-12 col-xs-12  header_service">
@@ -137,7 +171,7 @@
                             <button class="btn">
                               <div></div>
                                READ MORE
-                                
+
                             </button>
                         </div>
                     </div>
@@ -156,7 +190,7 @@
             </div>
         </section>
         <!-- services ending-->
-        
+
         <!-- sep1 start-->
         <section id="sep1">
            <div class="row">
@@ -175,7 +209,7 @@
                         <div class="divider"></div>
                         <span>READ MORE</span>
                    </div>
-                   
+
                 </div>
                 <div class="col-md-3 col-sm-6 sep_fruit_3">
                     <div class="fruit_slide_effect_03"></div>
@@ -196,7 +230,7 @@
             </div>
         </section>
         <!-- sep1 ending-->
-        
+
         <section id="coreValues">
             <div class="container">
                 <div class="col-md-8 col-sm-12 col-xs-12  header_core">
@@ -225,16 +259,16 @@
                 </div>
             </div>
         </section>
-       
-        
+
+
         <section id="gallery_header">
-            
+
             <h1>GALLERY</h1>
             <div class="divider"></div>
         </section>
-    
+
         <section id="gallery">
-        
+
             <div class="grid">
               <div class="grid-item">
                   <img src="img/gallery/page-1_img09_original.jpg" alt="">
@@ -268,26 +302,77 @@
                   <img src="img/gallery/page-1_img15_original.jpg" alt="">
                   <i class="fa fa-plus fa-3x"></i>
               </div>
-              
-              
+
+
             </div>
-            
-        </section>       
-       
-       
+
+        </section>
+
+
        <section id="gallery_header">
-            
+
             <button class="btn defaultButton">
                 VIEW ALL
             </button>
-            
+
         </section>
-    
-       
+        <section id="footer">
+          <div class="container">
+
+
+                  <div class="col-md-7">
+
+                      <div class="row">
+                        <h1> 8901 Marmora road,
+                            <br> Glasgow, D04 89GR <br>
+                            +1 (800) 2345 6789</h1>
+                            <div class="line">
+
+                            </div>
+                      </div>
+
+                    <div class="row">
+                       <h2>CONTACT US</h2>
+
+                    </div>
+                    <div class="row">
+                      <input type="text" name="name" value="" placeholder="Your name"><br>
+                      <input type="text" name="name" value="" placeholder="Your email"><br>
+                      <textarea name="textarea" rows="1" cols="40" placeholder="Your message"></textarea><br>
+                    </div>
+
+                    <div class="row">
+                      <button>SEND</button>
+                    </div>
+                    <div class="row">
+                      <span>Tramass © 2016 • Privacy Policy</span>
+                    </div>
+
+               </div>
+
+               <div class="col-md-5">
+                 <div class="row plan">
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26081603.294420466!2d-95.677068!3d37.06250000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1473521487781" width="370" height="420" frameborder="2" style="border:0" allowfullscreen></iframe>
+
+                 </div>
+                 <ul>
+                   <li><a href="www.facebook.com">Facebook</a></li>
+                   <li><a href="www.twitter.com">Twitter</a></li>
+                   <li><a href="www.youtube.com">YouTube</a></li>
+                   <li><a href="www.google.com">Google +</a></li>
+                   <li><a href="www.pinterest.com">Pinterest</a></li>
+
+                 </ul>
+
+               </div>
+
+          </div>
+         </section>
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="js/vendor/jquery-1.11.2.min.js"></script>
         <script src="js/vendor/bootstrap.min.js"></script>
-        
+
         <script src="https://unpkg.com/masonry-layout@4.1/dist/masonry.pkgd.min.js"></script>
         <script src="js/main.js"></script>
                <script>
@@ -297,13 +382,13 @@
 //        fitWidth: true
         });
         </script>
-        <script> 
-        
-               
-        
-        
-         
-            
+        <script>
+
+
+
+
+
+
 //        var windowWidth = $(window).width();
 //        function masonryLayout() {
 //            var blocks = document.querySelector("#gallery .row div")
@@ -318,31 +403,31 @@
 //                        blocks[i].style.top = newleft+"px";
 //                    }
 //                    newleft = (blocks[i-1].offsetLeft + blocks[i-1].offsetWidth);
-//                    blocks[i].style.left = newleft+"px";	
+//                    blocks[i].style.left = newleft+"px";
 //                }
 //            }
 //        }
 //        window.addEventListener("load", masonryLayout, false);
 //        window.addEventListener("resize", masonryLayout, false);
-//            
-            
-        
+//
 
-            
+
+
+
        function fruitSlide(hoverContent,slideeffect){
            $(hoverContent).mouseenter(function(){
                $(slideeffect).slideDown('300', function() {
                    $(this).fadeIn();
                });
            })
-           
+
            $(hoverContent).mouseleave(function(){
                $(slideeffect).slideUp('300', function() {
                    $(this).fadeOut();
                });
            })
-       } 
-       
+       }
+
        fruitSlide('.sep_fruit_1','.fruit_slide_effect_01')
        fruitSlide('.sep_fruit_2','.fruit_slide_effect_02')
        fruitSlide('.sep_fruit_3','.fruit_slide_effect_03')
@@ -358,20 +443,20 @@ $(document).scroll(function(){
     var wScroll = $(this).scrollTop();
 
     if(wScroll > $('.header_heading').offset().top){
-        
+
         $( "#scroll_navbar" ).fadeIn( "fast", function() {
             // Animation complete.
         });
-        
+
 	};
     if(wScroll < $('.header_heading').offset().top){
-        
+
         $( "#scroll_navbar" ).fadeOut( "fast", function() {
             // Animation complete.
         });
-        
+
 	};
-    
+
 })
 
 
--- signin.php
+++ signin.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js" lang=""> 
+<html class="no-js" lang="">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -10,31 +10,118 @@
         <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css"> <!--Diger yerde istifade olunsa yukleyerik -->
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
-    <body> 
-        <section id="signin">      
-        <div  class="container-fluid">
-            <div class="row">
-                <div class="col-md-4 col-md-offset-4">
-                    <h2>Sign in</h2>
-                    <p>Please enter your username and password</p>
-                    <form>
-                        <div class="form-group">
-                            <label for="inputEmail1">Username or Email</label>
-                            <input type="email" class="form-control" id="inputEmail1" placeholder="">
-                        </div>
-                        <div class="form-group">
-                            <label for="inputPassword1">Password</label>
-                            <input type="password" class="form-control" id="inputPassword1" placeholder="">
-                        </div>
-                        <a href="forgot_password.php">Forgot Password?</a><br>
-                        <input type="submit" value="SIGN IN" class="btn btn-success btn-lg text-center col-md-12 btn-2">
-                        <p class="text-center">OR</p>
-                        <button type="submit" class="btn btn-primary btn-lg text-center col-md-12 btn-2"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> LOGIN WITH FACEBOOK</a></button>
-                    </form>
+    <body>
+      <section id="header">
+            <div class="col-md-12">
+                <div class="row">
+                    <nav class="navbar">
+                          <div class="container-fluid">
+                                <div class="navbar-header">
+
+                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                        <a class="navbar-brand" href="#"><span>T</span></a>
+                                    </button>
+
+                                    <a class="hidden-xs  navbar-brand" href="#"><span>TRA</span>MASS</a>
+
+                                </div>
+
+                                <div class="collapse navbar-collapse" id="myNavbar">
+
+                                  <ul class="nav navbar-nav navbar-right">
+                                      <li class="active"><a href="index.php">HOME</a></li>
+                                      <li><a href="about.php">ABOUT US</a></li>
+                                      <li><a href="signin.php">LOG IN</a></li>
+                                      <li><a href="signup.php">SIGN UP</a></li>
+                                  </ul>
+
+                                </div>
+                          </div>
+                    </nav>
+                 </div>
+
+            </div>
+      </section>
+
+
+        <section id="signin">
+            <div  class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4">
+                        <h2>Sign in</h2>
+                        <p>Please enter your username and password</p>
+                        <form action="">
+                            <div class="form-group">
+                                <label for="inputEmail1">Username or Email</label>
+                                <input type="email" class="form-control" id="inputEmail1" placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPassword1">Password</label>
+                                <input type="password" class="form-control" id="inputPassword1" placeholder="">
+                            </div>
+                            <a href="forgot_password.php">Forgot Password?</a><br>
+                            <input type="submit" value="SIGN IN" class="btn btn-success btn-lg text-center col-md-12 btn-2">
+                            <p class="text-center">OR</p>
+                            <button type="submit" class="btn btn-primary btn-lg text-center col-md-12 btn-2"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> LOGIN WITH FACEBOOK</a></button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-        </section> 
+        </section>
+
+
+        <section id="footer">
+          <div class="container">
+
+
+                  <div class="col-md-7">
+
+                      <div class="row">
+                        <h1> 8901 Marmora road,
+                            <br> Glasgow, D04 89GR <br>
+                            +1 (800) 2345 6789</h1>
+                            <div class="line">
+
+                            </div>
+                      </div>
+
+                    <div class="row">
+                       <h2>CONTACT US</h2>
+
+                    </div>
+                    <div class="row">
+                      <input type="text" name="name" value="" placeholder="Your name"><br>
+                      <input type="text" name="name" value="" placeholder="Your email"><br>
+                      <textarea name="textarea" rows="1" cols="40" placeholder="Your message"></textarea><br>
+                    </div>
+
+                    <div class="row">
+                      <button>SEND</button>
+                    </div>
+                    <div class="row">
+                      <span>Tramass © 2016 • Privacy Policy</span>
+                    </div>
+
+               </div>
+
+               <div class="col-md-5">
+                 <div class="row plan">
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26081603.294420466!2d-95.677068!3d37.06250000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1473521487781" width="370" height="420" frameborder="2" style="border:0" allowfullscreen></iframe>
+
+                 </div>
+                 <ul>
+                   <li><a href="www.facebook.com">Facebook</a></li>
+                   <li><a href="www.twitter.com">Twitter</a></li>
+                   <li><a href="www.youtube.com">YouTube</a></li>
+                   <li><a href="www.google.com">Google +</a></li>
+                   <li><a href="www.pinterest.com">Pinterest</a></li>
+
+                 </ul>
+
+               </div>
+
+          </div>
+         </section>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="js/vendor/jquery-1.11.2.min.js"></script>
         <script src="js/vendor/bootstrap.min.js"></script>
